0ae0f31a505a87eac5bf50dcb0388fc5e9c2fec9
CL Bogart <clbogart3@outlook.com>
2022-01-21 08:48:03 -0700

    Add project files.

---
+++ SimpleCalculator_VB_CB/SimpleCalculator.vb
@@ -0,0 +1,49 @@
+﻿'Carson Bogart
+'RCET 0265
+'Spring 2022
+'Simple calculator
+'
+
+Option Explicit On
+Option Strict On
+Module SimpleCalculator
+
+    Sub Main()
+        'assign the variables below a type
+        Dim userInput As String
+        Dim number1 As Integer
+        Dim number2 As Integer
+
+        Console.WriteLine("Please enter a number")
+        'convert number1 to an integer
+        number1 = CInt(Console.ReadLine)
+
+        Console.WriteLine("Enter a second number")
+        'convert number2 to an integer
+        number2 = CInt(Console.ReadLine)
+
+        Console.WriteLine("Enter + or * to add or multiply")
+        'gives the user an option between operations
+        userInput = Console.ReadLine()
+
+        If userInput = "+" Then
+            'if "+" is entered then the line below will execute
+            Console.WriteLine(number1 & " + " & number2 & " = " & number1 + number2)
+        ElseIf userInput = "*" Then
+            'if "*" is entered then the line below will execute
+            Console.WriteLine(number1 & " * " & number2 & " = " & number1 * number2)
+        Else
+            'if "+" or "*" is not selected the line below will run
+            Console.WriteLine("You can't do that try again")
+
+        End If
+
+        Console.WriteLine("Press enter to exit this program")
+
+
+        'wait for user input
+        Console.ReadLine()
+
+    End Sub
+
+End Module
